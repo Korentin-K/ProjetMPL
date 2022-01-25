7c76278c569738e90ea017917278568e4655b895
Benjamin S <benjamin.stome@gmail.com>
2022-01-25 15:31:46 +0100

    ajout navbar + config bdd

--- diagramme.php
+++ diagramme.php
@@ -2,15 +2,20 @@
 require_once "fonctions.php";
 writeHeaderHtml("diagramme MPM",2);
 ?>
-
-<body class="container-fluid d-flex">
-    
-<?php 
-$nbrCol = 10;
-for($i=0;$i<$nbrCol;$i++){
-    addLevel($i);
-} ?>
-    
+<body class="">
+    <div class="container-fluid mx-0 px-0">
+        <?php writeNavBar(); ?>
+    </div>
+    <div class="row col-12 divlevel">
+        <div class="d-flex col-3">  </div>
+        <div class="d-flex col-9 displayDiagramme">
+        <?php 
+        $nbrCol = 10;
+        for($i=0;$i<$nbrCol;$i++){
+            addLevel($i);
+        } ?>
+        </div>
+    </div>
 </body>
 
 <?php writeFooterHtml(); ?>
