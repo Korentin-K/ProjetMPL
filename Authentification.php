8c584627f005927f7a4db5628a4de5b94ce8cbce
Korentin K <korentin.kolaczek@epsi.fr>
2022-01-27 09:53:38 +0100

    Mise à jour Authentification

--- Authentification.php
+++ Authentification.php
@@ -1,6 +1,9 @@
 <?php
-if(isset($_POST['identifiant'])) $id = $_POST['identifiant'];
-else $id = "";
+require_once'config/config_db.php'
+
+
+if(isset($_POST['identifiant'])) $nom = $_POST['identifiant'];
+else $nom = "";
 
 if(isset($_POST['passwordC'])) $passwordConnexion = $_POST['passwordC'];
 else $passwordConnexion = "";
@@ -8,11 +11,33 @@ else $passwordConnexion = "";
 if(isset($_POST['identifiantI'])) $passwordConnexion = $_POST['identifiantI'];
 else $passwordConnexion = "";
 
-if(isset($_POST['email'])) $passwordConnexion = $_POST['email'];
-else $passwordConnexion = "";
+if(isset($_POST['email'])) $email = $_POST['email'];
+else $email = "";
 
-if(isset($_POST['passwordI'])) $passwordConnexion = $_POST['passwordI'];
-else $passwordConnexion = "";
+if(isset($_POST['passwordI'])) $passwordInscription = $_POST['passwordI'];
+else $passwordInscription= "";
 
-if(isset($_POST['passwordI2'])) $passwordConnexion = $_POST['passwordI2'];
-else $passwordConnexion = "";
+if(isset($_POST['passwordI2'])) $passwordInscription2 = $_POST['passwordI2'];
+else $passwordInscription2 = "";
+
+if($id=!"")
+{
+	//$query= "SELECT * FROM `utilisateur` WHERE nom='$nom' and motdepasse='".hash('sha256', $passwordConnexion)."'";
+
+	 
+}
+
+else 
+{
+	$maRequete = $conn-> prepare("INSERT INTO utilisateur(nom,mail,motdepasse) VALUE (name,email,mdp)");
+	$nom = mysqli_real_escape_string($conn, $nom); 
+	$maRequete->bindParam('name', $nom);
+	$email = mysqli_real_escape_string($conn, $email); 
+	$maRequete->bindParam('email', $email);
+	if($passwordInscription==$passwordInscription2)
+	{
+		$leMotDePasse=password_hash($passwordInscription, PASSWORD_DEFAULT)
+		$maRequete->bindParam('mdp', $leMotDePasse);
+	}
+	
+}
