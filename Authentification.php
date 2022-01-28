cb5a4a79bda57f29b2c81e1f6a255194aaa6200e
Korentin K <korentin.kolaczek@epsi.fr>
2022-01-28 11:54:10 +0100

    Modification Authentification et lien PageConnexion

--- Authentification.php
+++ Authentification.php
@@ -1,6 +1,5 @@
 <?php
-require_once'config/config_db.php'
-
+require_once'config/Database.php';
 
 if(isset($_POST['identifiant'])) $nom = $_POST['identifiant'];
 else $nom = "";
@@ -8,8 +7,8 @@ else $nom = "";
 if(isset($_POST['passwordC'])) $passwordConnexion = $_POST['passwordC'];
 else $passwordConnexion = "";
 
-if(isset($_POST['identifiantI'])) $passwordConnexion = $_POST['identifiantI'];
-else $passwordConnexion = "";
+if(isset($_POST['identifiantI'])) $nomI = $_POST['identifiantI'];
+else $nomI = "";
 
 if(isset($_POST['email'])) $email = $_POST['email'];
 else $email = "";
@@ -20,24 +19,27 @@ else $passwordInscription= "";
 if(isset($_POST['passwordI2'])) $passwordInscription2 = $_POST['passwordI2'];
 else $passwordInscription2 = "";
 
-if($id=!"")
+if($nom!="")
 {
+
 	//$query= "SELECT * FROM `utilisateur` WHERE nom='$nom' and motdepasse='".hash('sha256', $passwordConnexion)."'";
-
-	 
+	 echo "test Connexion";
+	
+	
 }
-
-else 
-{
-	$maRequete = $conn-> prepare("INSERT INTO utilisateur(nom,mail,motdepasse) VALUE (name,email,mdp)");
-	$nom = mysqli_real_escape_string($conn, $nom); 
-	$maRequete->bindParam('name', $nom);
+if($nomI!="" and $nom=="") 
+{	
+	//on regarde si l'identifiant n'est pas déjà utilisé
+	//$maRequete = $conn-> exec("SELECT * FROM utilisateur WHERE nom='$nomI'");
+	$maRequete = $conn-> prepare("INSERT INTO utilisateur(nom_utilisateur,mail_utilisateur,mdp_utilisateur) VALUE (name,email,mdp)");
+	$nomI = mysqli_real_escape_string($conn, $nom); 
+	$maRequete->bindParam('name', $nomI);
 	$email = mysqli_real_escape_string($conn, $email); 
 	$maRequete->bindParam('email', $email);
 	if($passwordInscription==$passwordInscription2)
 	{
-		$leMotDePasse=password_hash($passwordInscription, PASSWORD_DEFAULT)
+		$leMotDePasse=password_hash($passwordInscription, PASSWORD_DEFAULT);
 		$maRequete->bindParam('mdp', $leMotDePasse);
 	}
-	
+	echo "test Inscription";
 }
